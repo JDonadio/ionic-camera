cb2940b80a257ce95362af4275bb02289d13c5db
JDonadio <javier.prog@gmail.com>
2020-07-23 18:58:54 -0300

    first commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Camera, CameraOptions } from '@ionic-native/camera/ngx';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +8,31 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  constructor(
+    private camera: Camera,
+  ) {}
 
+  private takeImageAndNavigate(sourceType) {
+    const options: CameraOptions = {
+      allowEdit: true,
+      quality: 100,
+      destinationType: this.camera.DestinationType.DATA_URL,
+      encodingType: this.camera.EncodingType.JPEG,
+      mediaType: this.camera.MediaType.PICTURE,
+      sourceType: sourceType,
+    }
+
+    this.camera.getPicture(options).then((imageData) => {
+      const img = 'data:image/jpeg;base64,' + imageData;
+      console.log('img:', img)
+    }, (err) => {
+      console.log('# Error al capturar la imagen', err);
+    });
+  }
+
+  selectSourceType(type: string) {
+    const sourceType = type == 'browse' ?
+      this.camera.PictureSourceType.PHOTOLIBRARY : this.camera.PictureSourceType.CAMERA;
+    this.takeImageAndNavigate(sourceType);
+  }
 }
